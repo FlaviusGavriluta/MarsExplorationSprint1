acf5d7a42fd33801e0594b90273b1219f37198e8
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-28 08:37:46 +0300

    unit tests for dimension calculator impl

--- src/main/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImpl.java
+++ src/main/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImpl.java
@@ -3,6 +3,10 @@ package com.codecool.marsexploration.calculators.service;
 public class DimensionCalculatorImpl implements DimensionCalculator {
     @Override
     public int calculateDimension(int size, int dimensionGrowth) {
+        // Check if the size is zero or negative or the growth is negative
+        if (size <= 0 || dimensionGrowth < 0) {
+            return -1;
+        }
         // Calculate the minimum dimension that can contain the given size
         int minimumDimension = (int) Math.ceil(Math.sqrt(size));
         // Add the dimension growth to the minimum dimension
---
+++ src/test/java/com/codecool/test/calculators/service/DimensionCalculatorImplTest.java
@@ -0,0 +1,49 @@
+package com.codecool.test.calculators.service;
+
+import com.codecool.marsexploration.calculators.service.DimensionCalculator;
+import com.codecool.marsexploration.calculators.service.DimensionCalculatorImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DimensionCalculatorImplTest {
+
+    private DimensionCalculator calculator = new DimensionCalculatorImpl();
+
+    @Test
+    public void testCalculateDimensionWithPositiveValues() {
+        assertEquals(8, calculator.calculateDimension(20, 3));
+        assertEquals(15, calculator.calculateDimension(20, 10));
+        assertEquals(35, calculator.calculateDimension(1000, 3));
+        assertEquals(453, calculator.calculateDimension(200000, 5));
+    }
+
+    @Test
+    public void testCalculateDimensionWithZeroSizeAndGrowth() {
+        assertEquals(0, calculator.calculateDimension(0, 0));
+    }
+
+    @Test
+    public void testCalculateDimensionWithZeroSizeAndPositiveGrowth() {
+        assertEquals(-1, calculator.calculateDimension(0, 1));
+        assertEquals(-1, calculator.calculateDimension(0, 10));
+    }
+
+    @Test
+    public void testCalculateDimensionWithPositiveSizeAndZeroGrowth() {
+        assertEquals(5, calculator.calculateDimension(20, 0));
+        assertEquals(32, calculator.calculateDimension(1000, 0));
+    }
+
+    @Test
+    public void testCalculateDimensionWithNegativeValues() {
+        assertEquals(-1, calculator.calculateDimension(-5, 3));
+        assertEquals(-1, calculator.calculateDimension(10, -2));
+    }
+
+    @Test
+    public void testCalculateDimensionWithEqualSizeAndGrowth() {
+        assertEquals(25, calculator.calculateDimension(20, 20));
+        assertEquals(1032, calculator.calculateDimension(1000, 1000));
+    }
+}
