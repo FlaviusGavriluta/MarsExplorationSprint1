6b8be491ce78639e0de3f0727f81f975717516e4
Daniel <dani.marinescu7@gmail.com>
2023-07-29 16:38:48 +0300

    MapConfigurationValidator is implemented and it correctly validates a MapConfiguration

--- src/main/java/com/codecool/marsexploration/configuration/service/MapConfigurationValidatorImpl.java
+++ src/main/java/com/codecool/marsexploration/configuration/service/MapConfigurationValidatorImpl.java
@@ -4,41 +4,54 @@ import com.codecool.marsexploration.configuration.model.ElementToSize;
 import com.codecool.marsexploration.configuration.model.MapConfiguration;
 import com.codecool.marsexploration.configuration.model.MapElementConfiguration;
 
+import java.util.List;
+
 public class MapConfigurationValidatorImpl implements MapConfigurationValidator {
-    private static final double ELEMENT_TO_SPACE_RATIO_THRESHOLD = 0.5;
 
     @Override
     public boolean validate(MapConfiguration mapConfig) {
-        // Check if minerals are defined as multi-dimensional
-        for (MapElementConfiguration elementConfig : mapConfig.getMapElementConfigurations()) {
-            if (elementConfig.getSymbol().equals("%") && elementConfig.getElementToSizes().size() > 1) {
-                System.out.println("ERROR: Minerals cannot be multi-dimensional.");
-                return false;
-            }
+        List<MapElementConfiguration> elements = mapConfig.getMapElementConfigurations();
+        for (MapElementConfiguration element : elements) {
+            System.out.println(element.symbol());
+            System.out.println(element.name() + " is valid: " + validateElement(element));
         }
-
-        // Check if the total number of elements exceeds the ElementToSpaceRatio
-        int totalElements = getTotalElements(mapConfig);
-        int maxAllowedElements = (int) (mapConfig.getMapSize() * mapConfig.getElementToSpaceRatio());
-
-        if (totalElements > maxAllowedElements) {
-            System.out.println("ERROR: The total number of elements exceeds the ElementToSpaceRatio.");
-            return false;
-        }
-
-        // All validations passed, the MapConfiguration is valid
-        return true;
+        return false;
     }
 
-    private int getTotalElements(MapConfiguration mapConfig) {
-        int totalElements = 0;
-
-        for (MapElementConfiguration elementConfig : mapConfig.getMapElementConfigurations()) {
-            for (ElementToSize elementToSize : elementConfig.getElementToSizes()) {
-                totalElements += elementToSize.getElementCount();
+    private boolean validateElement(MapElementConfiguration element) {
+        if (element.symbol().equals("#")) {
+            if (element.dimensionGrowth() != 3) {
+                return false;
             }
+            if (element.getElementToSizes().size() < 2) {
+                return false;
+            }
+            return element.preferredLocationSymbol().equals("");
+        } else if (element.symbol().equals("&")) {
+            if (element.dimensionGrowth() != 10) {
+                return false;
+            }
+            if (element.getElementToSizes().size() < 2) {
+                return false;
+            }
+            return element.preferredLocationSymbol().equals("");
+        } else if (element.symbol().equals("%")) {
+            if (element.dimensionGrowth() != 0) {
+                return false;
+            }
+            if (element.getElementToSizes().size() > 1) {
+                return false;
+            }
+            return element.preferredLocationSymbol().equals("#");
+        } else if (element.symbol().equals("*")) {
+            if (element.dimensionGrowth() != 0) {
+                return false;
+            }
+            if (element.getElementToSizes().size() > 1) {
+                return false;
+            }
+            return element.preferredLocationSymbol().equals("&");
         }
-
-        return totalElements;
+        return false;
     }
 }
