9af14abcf2a629e58cc6d0880a9b4421ee4dc559
Daniel <dani.marinescu7@gmail.com>
2023-07-29 16:06:58 +0300

    Created configuration objects

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -53,7 +53,38 @@ public class Application {
                 ""
         );
 
-        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg);
+        MapElementConfiguration pitsCfg = new MapElementConfiguration(
+                pitSymbol,
+                "pit",
+                List.of(
+                        new ElementToSize(2, 10),
+                        new ElementToSize(1, 20)
+                ),
+                10,
+                ""
+        );
+
+        MapElementConfiguration mineralsCfg = new MapElementConfiguration(
+                mineralSymbol,
+                "mineral",
+                List.of(
+                        new ElementToSize(10, 1)
+                ),
+                0,
+                mountainSymbol
+        );
+
+        MapElementConfiguration watersCfg = new MapElementConfiguration(
+                waterSymbol,
+                "water",
+                List.of(
+                        new ElementToSize(10, 1)
+                ),
+                0,
+                pitSymbol
+        );
+
+        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg, pitsCfg, mineralsCfg, watersCfg);
         return new MapConfiguration(1000, 0.5, elementsCfg);
     }
 }
