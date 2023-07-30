58d45ef9d6b0cd868549b30adc7da06c00bde685
Daniel <dani.marinescu7@gmail.com>
2023-07-30 21:12:45 +0300

    The MapElementPlacer interface is implemented

--- src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImpl.java
@@ -10,12 +10,37 @@ public class MapElementPlacerImpl implements MapElementPlacer {
         int elementHeight = element.getRepresentation()[0].length;
         int x = coordinate.x();
         int y = coordinate.y();
-        
-        return false;
+        int startX = x - (elementWidth / 2);
+        int startY = y - (elementHeight / 2);
+
+        if (startX < 0 || startY < 0 || startX + elementWidth > map.length || startY + elementHeight > map[0].length) {
+            return false;
+        }
+
+        for (int i = 0; i < elementWidth; i++) {
+            for (int j = 0; j < elementHeight; j++) {
+                if (!map[startX + i][startY + j].isEmpty() && !element.getRepresentation()[i][j].isEmpty()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     @Override
     public void placeElement(MapElement element, String[][] map, Coordinate coordinate) {
+        int elementWidth = element.getRepresentation().length;
+        int elementHeight = element.getRepresentation()[0].length;
+        int x = coordinate.x();
+        int y = coordinate.y();
+        int startX = x - (elementWidth / 2);
+        int startY = y - (elementHeight / 2);
 
+        for (int i = 0; i < elementWidth; i++) {
+            for (int j = 0; j < elementHeight; j++) {
+                map[startX + i][startY + j] = element.getRepresentation()[i][j];
+            }
+        }
     }
 }
