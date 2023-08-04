c59cdaedbfc952ef4b81dc3c70361cb3f78fa793
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-04 19:08:54 +0300

    refactored the dimension calculator impl class

--- src/main/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImpl.java
+++ src/main/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImpl.java
@@ -1,17 +1,11 @@
 package com.codecool.marsexploration.calculators.service;
 
 public class DimensionCalculatorImpl implements DimensionCalculator {
-
     @Override
     public int calculateDimension(int size, int dimensionGrowth) {
-        int dimension = 1;
-        int totalElements = dimension;
-
-        while (totalElements < size) {
-            dimension += dimensionGrowth;
-            totalElements = dimension * dimension;
-        }
-
-        return dimension;
+        // Calculate the minimum dimension that can contain the given size
+        int minimumDimension = (int) Math.ceil(Math.sqrt(size));
+        // Add the dimension growth to the minimum dimension
+        return minimumDimension + dimensionGrowth;
     }
 }
