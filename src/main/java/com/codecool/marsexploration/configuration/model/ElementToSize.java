884ca45915d926a8c23b3fccfe62da8ddb2ee2c3
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-04 14:46:49 +0300

    refactored element to size class

--- src/main/java/com/codecool/marsexploration/configuration/model/ElementToSize.java
+++ src/main/java/com/codecool/marsexploration/configuration/model/ElementToSize.java
@@ -1,7 +1,4 @@
 package com.codecool.marsexploration.configuration.model;
 
 public record ElementToSize(int elementCount, int size) {
-    public int getElementCount() {
-        return elementCount;
-    }
 }
