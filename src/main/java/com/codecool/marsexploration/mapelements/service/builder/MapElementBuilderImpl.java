203e1cf74dcc56ca17df57e78036db1e3e089a25
Daniel <dani.marinescu7@gmail.com>
2023-08-05 13:19:47 +0300

    Fixed MapElementBuilderImpl

--- src/main/java/com/codecool/marsexploration/mapelements/service/builder/MapElementBuilderImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/builder/MapElementBuilderImpl.java
@@ -1,5 +1,9 @@
 package com.codecool.marsexploration.mapelements.service.builder;
 
+import com.codecool.marsexploration.calculators.model.Coordinate;
+import com.codecool.marsexploration.calculators.service.CoordinateCalculator;
+import com.codecool.marsexploration.calculators.service.CoordinateCalculatorImpl;
+import com.codecool.marsexploration.calculators.service.DimensionCalculator;
 import com.codecool.marsexploration.calculators.service.DimensionCalculatorImpl;
 import com.codecool.marsexploration.mapelements.model.MapElement;
 
@@ -8,18 +12,31 @@ import java.util.Arrays;
 public class MapElementBuilderImpl implements MapElementBuilder{
     @Override
     public MapElement build(int size, String symbol, String name, int dimensionGrowth, String preferredLocationSymbol) {
-        DimensionCalculatorImpl dimensionCalculator = new DimensionCalculatorImpl();
+        DimensionCalculator dimensionCalculator = new DimensionCalculatorImpl();
+
         int dimension = dimensionCalculator.calculateDimension(size, dimensionGrowth);
-        String[][] representation = representation(symbol, dimension);
+        String[][] representation = representation(size, symbol, dimension);
 
         return new MapElement(representation, name, dimension, preferredLocationSymbol);
     }
 
-    private String[][] representation(String symbol, int dimension) {
+    private String[][] representation(int size, String symbol, int dimension) {
         String[][] representation = new String[dimension][dimension];
-        for (int i = 0; i <= dimension - 1; i++) {
-            for (int j = 0; j <= dimension - 1; j++) {
-                representation[i][j] = symbol;
+        CoordinateCalculator coordinateCalculator = new CoordinateCalculatorImpl();
+
+        for (int i = 0; i <= size; i++) {
+            Coordinate randomCoordinate = coordinateCalculator.getRandomCoordinate(dimension);
+            int x = randomCoordinate.getX();
+            int y = randomCoordinate.getY();
+
+            representation[x][y] = symbol;
+        }
+
+        for (int i = 0; i < representation.length; i++) {
+            for (int j = 0; j < representation[0].length; j++) {
+                if (representation[i][j] == null) {
+                    representation[i][j] = " ";
+                }
             }
         }
         return representation;
