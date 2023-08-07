86f1f613adb3768a628b9f9287384f3d46d36063
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-07 13:37:24 +0300

    add more tests

--- src/test/java/com/codecool/marsexploration/mapelements/service/generator/MapElementsGeneratorImplTest.java
+++ src/test/java/com/codecool/marsexploration/mapelements/service/generator/MapElementsGeneratorImplTest.java
@@ -30,3 +30,43 @@ public class MapElementsGeneratorImplTest {
         // ... additional assertions based on your configuration
     }
 }
+/*
+package com.codecool.marsexploration.mapelements.service.generator;
+
+import com.codecool.marsexploration.configuration.model.ElementToSize;
+import com.codecool.marsexploration.configuration.model.MapConfiguration;
+import com.codecool.marsexploration.configuration.model.MapElementConfiguration;
+import com.codecool.marsexploration.mapelements.model.MapElement;
+import com.codecool.marsexploration.mapelements.service.builder.MapElementBuilder;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapElementsGeneratorImplTest {
+
+    @Test
+    public void testCreateAll() {
+        MapElementBuilder mockBuilder = mock(MapElementBuilder.class);
+        when(mockBuilder.build(anyInt(), anyString(), anyString(), anyInt(), anyString())).thenReturn(new MapElement(new String[2][2], "name", 2));
+
+        List<MapElementConfiguration> elementConfigs = List.of(
+                new MapElementConfiguration("symbol", "name", List.of(new ElementToSize(2, 20)), 3, "")
+        );
+        MapConfiguration mapConfig = new MapConfiguration(17, 0.5, elementConfigs);
+
+        MapElementsGenerator generator = new MapElementsGeneratorImpl(mockBuilder);
+        Iterable<MapElement> elements = generator.createAll(mapConfig);
+
+        int count = 0;
+        for (MapElement element : elements) {
+            count++;
+        }
+
+        assertEquals(20, count);
+        verify(mockBuilder, times(20)).build(2, "symbol", "name", 3, "");
+    }
+}
+
+ */
--- src/test/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImplTest.java
+++ src/test/java/com/codecool/marsexploration/mapelements/service/placer/MapElementPlacerImplTest.java
@@ -1,36 +1,93 @@
 package com.codecool.marsexploration.mapelements.service.placer;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.codecool.marsexploration.calculators.model.Coordinate;
 import com.codecool.marsexploration.mapelements.model.MapElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MapElementPlacerImplTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Test
-    public void testCanPlaceElement() {
-        String[][] map = new String[5][5];
-        MapElement element = new MapElement(new String[][]{{"X"}}, "Test", 1);
-        Coordinate coordinate = new Coordinate(2, 2);
+class MapElementPlacerImplTest {
 
-        MapElementPlacerImpl placer = new MapElementPlacerImpl();
-        assertTrue(placer.canPlaceElement(element, map, coordinate));
+    private MapElementPlacer mapElementPlacer;
+    private String[][] map;
 
-        map[2][2] = "Y";
-        assertFalse(placer.canPlaceElement(element, map, coordinate));
+    @BeforeEach
+    void setUp() {
+        mapElementPlacer = new MapElementPlacerImpl();
+        map = new String[][] {
+                {" ", " ", " "},
+                {" ", " ", " "},
+                {" ", " ", " "}
+        };
     }
 
     @Test
-    public void testPlaceElement() {
-        String[][] map = new String[5][5];
-        MapElement element = new MapElement(new String[][]{{"X"}}, "Test", 1);
+    void canPlaceElement_True() {
+        MapElement element = new MapElement(new String[][] {{"#"}}, "test", 1);
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertTrue(mapElementPlacer.canPlaceElement(element, map, coordinate));
+    }
+
+    @Test
+    void canPlaceElement_False_OutOfBounds() {
+        MapElement element = new MapElement(new String[][] {{"#"}, {"#"}}, "test", 2);
         Coordinate coordinate = new Coordinate(2, 2);
+        assertFalse(mapElementPlacer.canPlaceElement(element, map, coordinate));
+    }
 
-        MapElementPlacerImpl placer = new MapElementPlacerImpl();
-        placer.placeElement(element, map, coordinate);
+    @Test
+    void canPlaceElement_False_OccupiedSpace() {
+        map[1][1] = "*";
+        MapElement element = new MapElement(new String[][] {{"#"}}, "test", 1);
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertFalse(mapElementPlacer.canPlaceElement(element, map, coordinate));
+    }
 
-        assertEquals("X", map[2][2]);
-        // ... additional assertions for multi-dimensional elements
+    @Test
+    void placeElement() {
+        MapElement element = new MapElement(new String[][] {{"#"}}, "test", 1);
+        Coordinate coordinate = new Coordinate(1, 1);
+        mapElementPlacer.placeElement(element, map, coordinate);
+        assertEquals("#", map[1][1]);
     }
 }
+
+
+
+//package com.codecool.marsexploration.mapelements.service.placer;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import com.codecool.marsexploration.calculators.model.Coordinate;
+//import com.codecool.marsexploration.mapelements.model.MapElement;
+//import org.junit.jupiter.api.Test;
+//
+//public class MapElementPlacerImplTest {
+//
+//    @Test
+//    public void testCanPlaceElement() {
+//        String[][] map = new String[5][5];
+//        MapElement element = new MapElement(new String[][]{{"X"}}, "Test", 1);
+//        Coordinate coordinate = new Coordinate(2, 2);
+//
+//        MapElementPlacerImpl placer = new MapElementPlacerImpl();
+//        assertTrue(placer.canPlaceElement(element, map, coordinate));
+//
+//        map[2][2] = "Y";
+//        assertFalse(placer.canPlaceElement(element, map, coordinate));
+//    }
+//
+//    @Test
+//    public void testPlaceElement() {
+//        String[][] map = new String[5][5];
+//        MapElement element = new MapElement(new String[][]{{"X"}}, "Test", 1);
+//        Coordinate coordinate = new Coordinate(2, 2);
+//
+//        MapElementPlacerImpl placer = new MapElementPlacerImpl();
+//        placer.placeElement(element, map, coordinate);
+//
+//        assertEquals("X", map[2][2]);
+//        // ... additional assertions for multi-dimensional elements
+//    }
+//}
