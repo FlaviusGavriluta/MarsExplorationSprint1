b4c63c02ccfb5fb89b8f49de4f211aa6a70918a4
Daniel <dani.marinescu7@gmail.com>
2023-08-08 13:52:46 +0300

    Removed duplicate classes

--- src/main/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImpl 2.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.marsexploration.calculators.service;
-
-public class DimensionCalculatorImpl implements DimensionCalculator{
-    @Override
-    public int calculateDimension(int size, int dimensionGrowth) {
-        return (int) Math.ceil(Math.sqrt(size)) + dimensionGrowth;
-    }
-}
