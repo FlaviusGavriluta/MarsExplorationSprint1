2f15def50543e28f2acfcec1033b119e9a7851a8
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-09 01:17:39 +0300

    refactor map configuration validator impl test class

--- src/test/java/com/codecool/marsexploration/configuration/service/MapConfigurationValidatorImplTest.java
+++ src/test/java/com/codecool/marsexploration/configuration/service/MapConfigurationValidatorImplTest.java
@@ -20,17 +20,48 @@ class MapConfigurationValidatorImplTest {
     }
 
     @Test
-    void testValidate_ValidConfiguration() {
-        MapConfigurationValidator validator = new MapConfigurationValidatorImpl();
+    void shouldValidateValidConfiguration() {
         MapConfiguration mapConfig = createValidConfiguration();
         assertTrue(validator.validate(mapConfig));
     }
 
     @Test
-    void testValidate_ExceedsElementToSpaceRatio() {
-        MapConfigurationValidator validator = new MapConfigurationValidatorImpl();
+    void shouldInvalidateWhenExceedingElementToSpaceRatio() {
         MapConfiguration mapConfig = createExceedingElementToSpaceRatioConfiguration();
         assertFalse(validator.validate(mapConfig));
+    }
+
+    @Test
+    void shouldValidateConfigurationWithFiftyPercentElementToSpaceRatio() {
+        MapElementConfiguration mountainsCfg = new MapElementConfiguration("#", "mountain", List.of(new ElementToSize(2, 20), new ElementToSize(1, 30)), 3, "");
+        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg);
+        MapConfiguration validMapConfig = new MapConfiguration(12, 0.5, elementsCfg);
+        assertTrue(validator.validate(validMapConfig));
+    }
+
+    @Test
+    void shouldInvalidateWhenExceedingElementToSpaceRatioInMountains() {
+        MapElementConfiguration mountainsCfg = new MapElementConfiguration("#", "mountain", List.of(new ElementToSize(2, 20), new ElementToSize(1, 30)), 3, "");
+        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg);
+        MapConfiguration invalidMapConfig = new MapConfiguration(15, 0.3, elementsCfg);
+        assertFalse(validator.validate(invalidMapConfig));
+    }
+
+    @Test
+    void shouldInvalidateMultiDimensionalMinerals() {
+        MapElementConfiguration mineralsCfg = new MapElementConfiguration("%", "mineral", List.of(new ElementToSize(1, 2) // Invalid dimension size for mineral
+        ), 0, "#");
+        List<MapElementConfiguration> elementsCfg = List.of(mineralsCfg);
+        MapConfiguration invalidMapConfig = new MapConfiguration(10, 0.5, elementsCfg);
+        assertFalse(validator.validate(invalidMapConfig));
+    }
+
+    @Test
+    void shouldInvalidateMultiDimensionalWaterWithGrowth() {
+        MapElementConfiguration mineralsCfg = new MapElementConfiguration("*", "water", List.of(new ElementToSize(1, 1)), 3, "");
+        List<MapElementConfiguration> elementsCfg = List.of(mineralsCfg);
+        MapConfiguration invalidMapConfig = new MapConfiguration(10, 0.5, elementsCfg);
+        assertFalse(validator.validate(invalidMapConfig));
     }
 
     private MapConfiguration createValidConfiguration() {
@@ -47,52 +78,4 @@ class MapConfigurationValidatorImplTest {
         List<MapElementConfiguration> mapElementConfigurations = List.of(mineral);
         return new MapConfiguration(10, 0.1, mapElementConfigurations);
     }
-
-    @Test
-    void testValidMapConfiguration() {
-        // Create a valid map configuration with 50% ElementToSpaceRatio
-        MapElementConfiguration mountainsCfg = new MapElementConfiguration("#", "mountain", List.of(new ElementToSize(2, 20), new ElementToSize(1, 30)), 3, "");
-
-        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg);
-        MapConfiguration validMapConfig = new MapConfiguration(12, 0.5, elementsCfg);
-
-        assertTrue(validator.validate(validMapConfig));
-    }
-
-    @Test
-    void testInvalidMapConfigurationExceedingElementToSpaceRatio() {
-        // Create an invalid map configuration with more elements than allowed by ElementToSpaceRatio
-        MapElementConfiguration mountainsCfg = new MapElementConfiguration("#", "mountain", List.of(new ElementToSize(2, 20), new ElementToSize(1, 30)), 3, "");
-
-        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg);
-        MapConfiguration invalidMapConfig = new MapConfiguration(15, 0.3, elementsCfg);
-
-        assertFalse(validator.validate(invalidMapConfig));
-    }
-
-    @Test
-    void testInvalidMapConfigurationMultiDimensionalMinerals() {
-        // Create an invalid map configuration with multi-dimensional minerals
-        MapElementConfiguration mineralsCfg = new MapElementConfiguration("%", "mineral", List.of(new ElementToSize(1, 2) // Invalid dimension size for mineral
-        ), 0, "#");
-
-        List<MapElementConfiguration> elementsCfg = List.of(mineralsCfg);
-        MapConfiguration invalidMapConfig = new MapConfiguration(10, 0.5, elementsCfg);
-
-        assertFalse(validator.validate(invalidMapConfig));
-    }
-
-    // add more tests here
-    @Test
-    void testInvalidMapConfigurationMultiDimensionalWater() {
-        // Create an invalid map configuration with multi-dimensional water
-        MapElementConfiguration mineralsCfg = new MapElementConfiguration("*", "water", List.of(new ElementToSize(1, 1)), 3, // Invalid dimension growth for water
-                "");
-
-        List<MapElementConfiguration> elementsCfg = List.of(mineralsCfg);
-        MapConfiguration invalidMapConfig = new MapConfiguration(10, 0.5, elementsCfg);
-
-        assertFalse(validator.validate(invalidMapConfig));
-    }
-
 }
